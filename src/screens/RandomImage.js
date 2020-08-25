646a6571de97cc4b160a41d6d239baf11b3938f5
Yididya Samuel <yidisam18@gmail.com>
2020-08-25 14:46:38 -0400

    check for textInputs & cleanups

--- src/screens/RandomImage.js
+++ src/screens/RandomImage.js
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
-import {  View,
+import {
+  View,
   Text,
   StyleSheet,
   ImageBackground,
@@ -7,31 +8,38 @@ import {  View,
   Dimensions,
   TouchableOpacity,
   Alert,
-  Image
-  } from 'react-native';
+  Image,
+} from 'react-native';
 import * as MediaLibrary from 'expo-media-library';
-import { Entypo, Ionicons, FontAwesome } from "@expo/vector-icons";
-import ViewShot from "react-native-view-shot";
-import * as ImageManipulator from "expo-image-manipulator";
-import * as Sharing from 'expo-sharing'
-import constants from "expo-constants";
+import { Entypo, Ionicons, FontAwesome } from '@expo/vector-icons';
+import ViewShot from 'react-native-view-shot';
+import * as ImageManipulator from 'expo-image-manipulator';
+import * as Sharing from 'expo-sharing';
+import constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 
-const saveImg = async (uri, setUri, viewShotRef, navigation) => {
-  const img = await viewShotRef.current.capture();
+const saveImg = async (setTopText, setBottomText, viewShotRef, navigation) => {
+  try {
+    const img = await viewShotRef.current.capture();
+    const { granted } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (granted) {
+      const granted = await MediaLibrary.getPermissionsAsync();
+      console.log(granted);
 
-            await setUri(img);      
-            const granted = await MediaLibrary.getPermissionsAsync()
-            if (granted) {
-                  await MediaLibrary.saveToLibraryAsync(img);
-                  console.log('Saved!')
-                  alert('saved to Gallery!')
-                  navigation.navigate('Home')
-
-            } else {
-              Alert.alert("Permission is required");
-            }
-}
-const openShareDialogAsync = async (viewShotRef) => {
+      await MediaLibrary.saveToLibraryAsync(img);
+      console.log('Saved!');
+      setTopText('');
+      setBottomText('');
+      alert('saved to Gallery!');
+      navigation.navigate('Home');
+    } else {
+      Alert.alert('Permission is required');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+const openShareDialogAsync = async (viewShotRef, setBottomText, setTopText) => {
   const img = await viewShotRef.current.capture();
   if (!(await Sharing.isAvailableAsync())) {
     alert(`Uh oh, sharing isn't available on your platform`);
@@ -40,23 +48,25 @@ const openShareDialogAsync = async (viewShotRef) => {
   try {
     const manipResult = await ImageManipulator.manipulateAsync(img);
     await Sharing.shareAsync(manipResult.uri);
+    setTopText('');
+    setBottomText('');
   } catch (err) {
-    alert(err)
-    console.log("ERROR: ", err)
+    alert(err);
+    console.log('ERROR: ', err);
   }
-}; 
+};
 
-const RandomImage = ({navigation}) => {
+const RandomImage = ({ navigation }) => {
+  const viewShotRef = useRef(null);
+  const [img, setImg] = useState('https://i.imgflip.com/345v97.jpg');
+  const [allMemes, setAllMemes] = useState([]);
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
 
-  const viewShotRef = useRef(null); 
-  const [img, setImg] = useState('https://i.imgflip.com/345v97.jpg')
-  const [allMemes, setAllMemes] = useState([])
-
-
-  const changeImg = async (setImg) => {
-         const randNum = Math.floor(Math.random() * allMemes.length)
-      console.log(allMemes.length)
-      setImg(allMemes[randNum].url)
+  const changeImg = async setImg => {
+    const randNum = Math.floor(Math.random() * allMemes.length);
+    console.log(allMemes.length);
+    setImg(allMemes[randNum].url);
   };
 
   const { width, height } = Dimensions.get('window');
@@ -65,128 +75,154 @@ const RandomImage = ({navigation}) => {
     fetch('https://api.imgflip.com/get_memes')
       .then(res => res.json())
       .then(res => {
-        const { memes } = res.data 
-        setAllMemes(memes)
-      })
+        const { memes } = res.data;
+        setAllMemes(memes);
+      });
   });
 
-  
   return (
-    <View style={{ flex: 1 }}>
-        <ViewShot ref={viewShotRef}  options={{ format: "jpg", quality: 0.9 }} style={{ flex: 7}} >
+    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+      <ViewShot
+        ref={viewShotRef}
+        options={{ format: 'jpg', quality: 0.9 }}
+        style={{ flex: 7 }}
+      >
+        <TextInput
+          placeholder="Enter Top Text"
+          placeholderTextColor="#000"
+          textAlign="center"
+          multiline={true}
+          maxLength={25}
+          autoCapitalize="characters"
+          onKeyPress={() => {}}
+          returnKeyType="done"
+          onChangeText={text => setTopText(text)}
+          style={{
+            flex: 1,
+            padding: 18,
+            // position: "absolute",
+            // top: 10,
+            width,
+            textAlign: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#000',
+            backgroundColor: '#fff',
+          }}
+        />
 
-            <TextInput
-                  placeholder='Enter Top Text'
-                  placeholderTextColor='#000'
-                  textAlign='center'
-                  multiline={true}
-                  maxLength={25}
-                  autoCapitalize = 'characters'
-                  onKeyPress = {() => {}}
-                  returnKeyType = 'done'
-                  style={{
-                    flex:1,
-                    padding: 18,
-                    // position: "absolute",
-                    // top: 10,
-                    width,
-                    textAlign: "center",
-                    alignItems: "center",
-                    fontSize: 40,
-                    color: '#000',
-                    backgroundColor: '#fff',
-                    marginBottom: 5
-                  }}
-                />
-  
-            
-            <Image  
-              style={{
-                flex: 5,
-                  width: width,
-                  height: height -60 - constants.statusBarHeight,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  overflow:'hidden'
-              }}  source={{uri: img}}  resizeMode="contain" 
-              resizeMethod="resize"
-            />
-          
-            <TextInput
-                  placeholder='Enter Bottom Text'
-                  placeholderTextColor='#000'
-                  textAlign='center'
-                  multiline={true}
-                  maxLength={25}
-                  autoCaptialize = 'characters'
-                  style={{
-                    flex:1,
-                    padding: 18,
-                    // position: "absolute",
-                    // bottom: 10,
-                    width,
-                    textAlign: "center",
-                    alignItems: "center",
-                    fontSize: 40,
-                    color: '#000',
-                    backgroundColor: '#fff'
-                  }}
-                />
-            
-        </ViewShot>
-      <View style={{ flex: 1, flexDirection: "row", justifyContent: 'space-around' }}>
-        <TouchableOpacity style={{ flex: 1, backgroundColor: "#39f70a",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 13,
-          margin: 4,
-          width: 5,
-          height: 50,
-          padding: 0
-            }}onPress={ () => changeImg(setImg)} >
-        <FontAwesome name="rotate-right" size={24} color="#fff" />
-          {/* <Text>Change Image</Text> */}
+        <Image
+          style={{
+            flex: 5,
+            width: width,
+            height: height - 60 - constants.statusBarHeight,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          source={{ uri: img }}
+          resizeMode="contain"
+          resizeMethod="resize"
+        />
+
+        <TextInput
+          placeholder="Enter Bottom Text"
+          placeholderTextColor="#000"
+          textAlign="center"
+          multiline={true}
+          maxLength={25}
+          autoCaptialize="characters"
+          onChangeText={text => setBottomText(text)}
+          style={{
+            flex: 1,
+            padding: 18,
+            // position: "absolute",
+            // bottom: 10,
+            width,
+            textAlign: 'center',
+            alignItems: 'center',
+            fontSize: 40,
+            color: '#000',
+            backgroundColor: '#fff',
+          }}
+        />
+      </ViewShot>
+
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+        }}
+      >
+        <TouchableOpacity
+          style={styles.random}
+          onPress={() => changeImg(setImg)}
+        >
+          <FontAwesome name="rotate-right" size={24} color="#fff" />
         </TouchableOpacity>
         <TouchableOpacity
-          style={styles.btn}
-          onPress={() => saveImg(img, setImg, viewShotRef, navigation)}
+          style={styles.save}
+          onPress={() =>
+            topText && bottomText
+              ? saveImg(img, setImg, viewShotRef, navigation)
+              : null
+          }
         >
-          <Entypo name='save' size={22} color="#fff" style={{ marginRight: 5 }} />
-          <Text style={{color:"#fff"}}>Save Image</Text>
+          <Entypo
+            name="save"
+            size={22}
+            color="#fff"
+            style={{ marginRight: 5 }}
+          />
+          <Text style={{ color: '#fff' }}>Save Image</Text>
         </TouchableOpacity>
+
         <TouchableOpacity
-          style={{ flex: 1, backgroundColor: "#39f70a",
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 13,
-          margin: 4,
-          width: 8,
-          height: 50,
-          padding: 0
-            }}
-            onPress={() => { 
-              console.log('Pressed')
-              openShareDialogAsync(viewShotRef)
-              } }
+          style={styles.share}
+          onPress={() => {
+            topText && bottomText
+              ? openShareDialogAsync(viewShotRef, setBottomText, setTopText)
+              : null;
+          }}
         >
-         <Ionicons name="md-share-alt" size={25} color="white" 
-         />
+          <Ionicons name="md-share-alt" size={25} color="white" />
         </TouchableOpacity>
       </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  btn: {
-    width: 130,
-    height: 50,
-    backgroundColor: "#39f70a",
-    justifyContent: "center",
-    alignItems: "center",
+  random: {
+    flex: 1,
+    backgroundColor: '#39f70a',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 13,
     margin: 4,
-    flexDirection: 'row'
-    // padding: 8,
+    width: 5,
+    height: 50,
+    padding: 0,
+  },
+  save: {
+    width: 130,
+    height: 50,
+    backgroundColor: topText && bottomText ? '#39f70a' : '#b6f285',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 13,
+    margin: 4,
+    flexDirection: 'row',
+  },
+  share: {
+    flex: 1,
+    backgroundColor: topText && bottomText ? '#39f70a' : '#b6f285',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 13,
+    margin: 4,
+    width: 8,
+    height: 50,
+    padding: 0,
   },
 });
 

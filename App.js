ea392f7efbdce587a7df42be198a1054939b1e80
Yididya Samuel <yidisam18@gmail.com>
2020-09-14 17:55:01 -0400

    changed name & removed unused codes

--- App.js
+++ App.js
@@ -1,50 +1,38 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import constants from "expo-constants";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import constants from 'expo-constants';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from "./src/screens/Home";
-import Edit from "./src/screens/Edit";
-import RandomImage from "./src/screens/RandomImage";
+import Home from './src/screens/Home';
+import Edit from './src/screens/Edit';
+import RandomImage from './src/screens/RandomImage';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-  <NavigationContainer style={{ flex: 1 }}>
+    <NavigationContainer style={{ flex: 1 }}>
       <View style={styles.topBar} />
       <Stack.Navigator headerMode="none">
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Edit" component={Edit} />
-      <Stack.Screen name="Random" component={RandomImage} />
-    </Stack.Navigator>
-  </NavigationContainer>
-
-  )
-}
-export default App
-
-// export default function App() {
-//   return (
-//     <View style={{ flex: 1 }}>
-//       <View style={styles.topBar} />
-//       {/* <Edit /> */}
-//       <Home />
-      
-//     </View>
-//   );
-// }
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Edit" component={Edit} />
+        <Stack.Screen name="Random" component={RandomImage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   topBar: {
     height: constants.statusBarHeight,
-    backgroundColor: "#39f70a",
+    backgroundColor: '#39f70a',
   },
 });
